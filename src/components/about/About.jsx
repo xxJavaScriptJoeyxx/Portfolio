c499df9b3972c263c1abedfa8bae312bd4eda173
xxJavaScriptJoeyxx <jomatteucci80@gmail.com>
2023-10-06 11:55:00 -0700

    Tools

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,24 +1,25 @@
 import React from "react"
-import me from "../about/me.jpg"
+// import me from "../about/me.jpg"
 
 const About = () => {
     
     return (
 
-       
+        <div className="aboutContainer">
         <div id="aboutDiv">
 
-<p id="joeyTitle">What's Joey About?</p>
-            <div id="imgDiv"><img src={me} alt="pic of me" /></div>
+            <div className="aboutTitleContainer"><p id="joeyTitle">What's Joey About?</p>
+            </div>
+           
             
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+            <p id="aboutPara">Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
                 Consequuntur voluptas eveniet magni voluptatum ratione in ab reiciendis totam, 
                 asperiores minima doloribus! Aliquid, nostrum. 
                 Possimus suscipit molestias porro consequuntur esse nemo dolorem laudantium blanditiis labore. 
                 Animi quam voluptatem hic temporibus repellendus facilis quos aut, fuga porro amet incidunt illum maxime commodi pariatur?
                 In minus tempore error similique qui at porro iusto.</p>
 
-        
+    </div>
         </div>
     )
 }
--- src/components/landing/Landing.jsx
+++ src/components/landing/Landing.jsx
@@ -1,23 +1,35 @@
-import react from "react"
+import React from "react"
 import {useState, useEffect} from "react"
 import "../landing/landing.css"
 import owlPic from "../../owlPic.png"
 import NameTitle from "../name/NameTitle"
 import Title from "../name/Title"
 import About from "../about/About"
+import Tools from "../tools/Tools"
+import me from "../about/me.jpg"
 
 const Landing = () => {
     return (
         <>
+        <div className="centered">
         <div id="picContainer">
-            <img id="binaryOwl" src={owlPic} alt="binary owl pic"/>
+            <img id="binaryOwl" className="binaryOwl" src={owlPic} alt="binary owl pic"/>
             </div>
-      <div id="nameTitle"><NameTitle text="Joey Matteucci"/></div> 
+            <div className="nameTitleContainer">
+                <div className="nameTitle"><NameTitle text="Josephine Matteucci" /></div>
+            </div>
+            <div className="imgContainer">
+            <div id="imgDiv"><img src={me} alt="pic of me" /></div>
+            </div>
+            <div className="titleContainer">
        <div id="title"><Title text="< Web Developer />"/></div>
+       </div>
+       
        <div id="aboutDiv"><About /></div>
-    
+     
+               <Tools/>
         
-      
+      </div>
         </>
     )
 }
